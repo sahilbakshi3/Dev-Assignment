fd6086c105f9fdecfbe89a171fc2a065e1c75e14
unknown <sahilbakshi3@example.com>
2025-10-07 12:44:37 +0530

    juspay interview question asked to me

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import DesktopNestedDrawer from "./components/DesktopNestedDrawer";
 
 function App() {
+
+  const [isExpanded, setIsExpanded] = useState(false);
+
   return (
     <div
       style={{
@@ -13,7 +16,23 @@ function App() {
         textAlign: "center"
       }}
     >
-      <DesktopNestedDrawer width={520} />
+      {/* <DesktopNestedDrawer width={520} /> */}
+      <div style = {{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItmes: 'center', height: '100vh' }}>
+        <div 
+          style={{
+            width: '200px',
+            height: isExpanded ? '400px' : '200px',
+            backgroundColor: 'red',
+            transition: 'height 0.3s ease-in-out' 
+          }}
+        >
+
+        </div>
+      </div>
+      <button onClick={() => {
+        setIsExpanded(!isExpanded)
+      }}>Click me</button>
+
     </div>
   );
 }
